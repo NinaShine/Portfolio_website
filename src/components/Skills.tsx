de72a98a88fb731ac85f8e6b14bbb2439492c7ed
NinaShine <salhinina2002@gmail.com>
2025-03-16 14:20:17 +0100

    maj skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,21 +4,30 @@ const Skills = () => {
   const skillCategories = [
     {
       title: 'Langages de programmation',
-      skills: ['C', 'C++', 'Java', 'Python', 'Pascal', 'Kotlin', 'JavaScript', 'HTML', 'CSS']
+      skills: ['C', 'C++', 'Java', 'Python', 'Pascal', 'Kotlin', 'JavaScript', 'TypeScript', 'HTML', 'CSS', 'SQL']
     },
     {
       title: 'Frameworks & Bibliothèques',
-      skills: ['React', 'Node.js', 'Flask', 'Machine Learning', 'Deep Learning']
+      skills: ['React', 'Node.js', 'Flask', 'Angular', 'Vue.js', 'Spring Boot', 'Django', 'Machine Learning', 'Deep Learning', 'TensorFlow', 'PyTorch', 'Scikit-learn']
     },
     {
       title: 'Outils & Technologies',
-      skills: ['Git', 'Docker', 'Kubernetes', 'Terraform', 'CI/CD', 'Big Data (HADOOP)']
+      skills: ['Git', 'Docker', 'Kubernetes', 'Terraform', 'CI/CD', 'Big Data (HADOOP, Spark, Snowflake)', 'Power BI', 'Business Object', 'Cloud Computing (AWS, Azure, GCP)', 'JIRA']
     },
     {
       title: 'Bases de données',
-      skills: ['MySQL', 'Oracle']
+      skills: ['MySQL', 'Oracle', 'SQL Server', 'PostgreSQL', 'MongoDB']
+    },
+    {
+      title: 'Méthodologies & Gestion de projet',
+      skills: ['Agile (Scrum, Kanban)', 'Gestion de projet', 'Analyse des besoins', 'Reporting automatisé', 'Rédaction de documentations techniques']
+    },
+    {
+      title: 'IA Générative & NLP',
+      skills: ['LLM (Large Language Models)', 'Embeddings', 'RAG (Retrieval-Augmented Generation)', 'Modèles Transformers']
     }
   ];
+
 
   return (
     <section id="skills" className="skills-section">
